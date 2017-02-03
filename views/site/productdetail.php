88ac1bb011df91656b8e2bb48672de0db450795f
devikaSn <devika@qburst.com>
2017-02-03 16:14:14 +0530

    Fixed the product detail ui

--- views/site/productdetail.php
+++ views/site/productdetail.php
@@ -21,20 +21,27 @@ use app\models\Adsinfo;
     	</div>
 
     	<div class="product-detail-description">
+               <br><br><br>
                 <h4>
                     <b style="color:#00628B"> Price: </b> <?php echo $product['price'];?><br><br>
                     <b style="color:#00628B"> Product Description: </b>
 
     	 			<p> 
                          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                        <?php echo $product['adDescription']; ?>
+                         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure d
+                        <!-- <?php echo $product['adDescription']; ?> -->
                         <br><br>
     	 			</p>
                         <b style="color:#00628B"> Contact Seller:</b><br>
-                        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                        
                         <?php echo  $product['contactAddress']; ?><br>
-                        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                        Phone: <?php echo $product['contactNumber']; ?>
+                            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                        <br><br>
+
+                         <b style="color:#00628B"> Phone:</b> <br>
+                          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                        <?php echo $product['contactNumber']; ?>
                 </h4>
     	</div>
     </div>
